fabdd740c3057901bed7a4460807c0f57ee805a9
Genkas design <gentilkambalekasyenene@gmail.com>
2026-07-19 16:30:54 +0200

    Switch Laravel database to PostgreSQL Render

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => 'mysql',
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -81,17 +81,22 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+            'url' => env('DATABASE_URL'),
+
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
+
             'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
+            'username' => env('DB_USERNAME', 'postgres'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
+
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
+
             'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
+
+            'sslmode' => env('DB_SSLMODE', 'require'),
         ],
 
         'sqlsrv' => [
