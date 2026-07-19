f95a609b3ca4eada880f0d49d58515b4af47505e
Genkas design <gentilkambalekasyenene@gmail.com>
2026-07-19 17:56:37 +0200

    Fix Tailwind custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,28 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
-/** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/js/**/*.js',
     ],
 
     theme: {
         extend: {
+            colors: {
+                'brand-red': '#E63946',
+                'brand-redDark': '#C62828',
+            },
+
             fontFamily: {
                 sans: ['Inter', ...defaultTheme.fontFamily.sans],
             },
-            colors: {
-                brand: {
-                    red: '#E63946', // Rouge vif moderne
-                    redDark: '#C62828', // Rouge plus foncé pour le hover
-                }
-            }
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+    ],
 };
